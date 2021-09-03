413f8334823ffd10e69f7b59da41714998190caf
Илья <82406580+IlliaRodionov@users.noreply.github.com>
2021-09-03 21:15:30 +0300

    Delete blog/backend/posts/static/js directory

--- blog/backend/posts/static/js/script.js
+++
@@ -1,7 +0,0 @@
-var myModal = document.getElementById('myModal')
-
-myModal.addEventListener('show.bs.modal', function (event) {
-  if (!data) {
-    return event.preventDefault() // останавливает отображение модального окна
-  }
-})
